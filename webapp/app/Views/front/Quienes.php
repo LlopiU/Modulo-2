10971038e0bda2528f8551fa85cbf6000e63032d
LlopiU <llopiulisesalan10@gmail.com>
2023-11-08 23:47:36 -0300

    webapp

---
+++ webapp/app/Views/front/Quienes.php
@@ -0,0 +1,32 @@
+﻿<body class="gradient-custom">
+        <h1 style="text-align: center;">QUIENES SOMOS</h1>
+        <div class="container">   
+          <p class="lead">   TecnoPanda nació de la necesidad de ofrecer una experiencia
+             de compra de componentes de PC superior. Un grupo de apasionados
+              entusiastas de la tecnología se reunió y decidió cambiar la forma 
+              en que las personas acceden a piezas de alta calidad. Con un plan
+               de negocios sólido y un enfoque en la calidad y el servicio al cliente,
+                abrimos nuestras puertas para brindar soluciones tecnológicas confiables a nuestros clientes.
+                 Desde entonces, hemos crecido y ampliado nuestro alcance para convertirnos en una fuente confiable
+                  de componentes de PC y asesoramiento técnico en todo el país.</p>
+    <div class="card-group mt-3">
+
+      <div class="card text-center border-info">
+        <div class="card-body bg-dark" style="border-radius: 0.4rem;" >
+        <img src="assets/img/perfil2.enc" class="" alt="..." class="img-fluid">
+        <h4 class="btn btn-warning font-weight-bold text-white">Estudiante</h4>
+                            <h4 class="btn btn-primary">UTN</h4>
+                            <h3 class="text-center text-white" ">Llopi Ulises Alan</h3>
+          <p class="card-text" >Soy un apasionado de la tecnología y un entusiasta del desarrollo web.
+                                Me encanta aprender y crear cosas nuevas. Mi experiencia incluye el diseño y desarrollo
+                                de sitios web utilizando tecnologías como HTML, CSS y JavaScript. También tengo
+                                experiencia en la creación de aplicaciones web dinámicas con frameworks como React, Angular y Next.js, tambien en frameworks como Bootstrap y
+                                Tailwind. </p>
+        </div>
+      </div>          
+      
+
+
+    </div>
+  </div>  
+    </body>
